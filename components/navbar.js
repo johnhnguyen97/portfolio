099c8d0dea074fdda56cc48769b1f1e4c5a968c5
John Nguyen <john.nguyen7991@gmail.com>
2025-03-12 14:53:37 -0500

    fix: Update navbar and theme toggle button layout
    - Fix theme toggle button and menu spacing
    
    - Fix navbar mobile responsiveness
    
    - Clean up menu structure
    
    - Update file organization

--- components/navbar.js
+++ components/navbar.js
@@ -3,6 +3,7 @@ import NextLink from 'next/link'
 import {Container, Box, Link, Heading, Flex, Menu, MenuItem, MenuList, MenuButton, IconButton, useColorModeValue, Stack } from '@chakra-ui/react'
 import { HamburgerIcon } from '@chakra-ui/icons'
 import Head from 'next/head'
+import ThemeToggleButton from './theme-toggle-button'
 
 const LinkItem = ({ href, path, children }) => {
 const active = path === href
@@ -30,71 +31,73 @@ const Navbar = props => {
                 <title>John Nguyen - Homepage</title>
             </Head>
             <Box
-            position="fixed"
-            as="nav"
-            w="100%"
-            bg={useColorModeValue('#ffffff40', '#20202380')}
-            style={{backdropFilter: 'blur(10px)'}}
-            zIndex={1}
-            {...props}
-        >
-            <Container
-                display="flex"
-                p={2}
-                maxW="container.md"
-                wrap="wrap"
-                align="center"
-                justify="space-between"
+                position="fixed"
+                as="nav"
+                w="100%"
+                bg={useColorModeValue('#ffffff40', '#20202380')}
+                style={{backdropFilter: 'blur(10px)'}}
+                zIndex={1}
+                {...props}
             >
-                <Flex align="center" mr={5}>
-                    <Heading as="h1" size="lg" letterSpacing="tighter" ml={3}>
-                        <Logo />
-                    </Heading>
-                </Flex>
-
-                <Stack
-                    direction={{ base: 'column', md: 'row' }}
-                    display={{ base: 'none', md: 'flex' }}
-                    width={{ base: 'full', md: 'auto' }}
-                    alignItems="center"
-                    flexGrow={1}
-                    mt={{ base: 4, md: 0 }}
+                <Container
+                    display="flex"
+                    p={2}
+                    maxW="container.md"
+                    wrap="wrap"
+                    align="center"
+                    justify="space-between"
                 >
-                    <LinkItem href="/about" path={path}>
-                        About
-                    </LinkItem>
-                    <LinkItem href="/works" path={path}>
-                        Works
-                    </LinkItem>
-                    <LinkItem href="/posts" path={path}>
-                        Posts
-                    </LinkItem>
-                </Stack>
+                    <Flex align="center" mr={5}>
+                        <Heading as="h1" size="lg" letterSpacing="tighter" ml={3}>
+                            <Logo />
+                        </Heading>
+                    </Flex>
 
-                <Box flex={1} align="right">
-                    <Menu isLazy id="navbar-menu">
-                        <MenuButton
-                            as={IconButton}
-                            icon={<HamburgerIcon />}
-                            variant="outline"
-                            aria-label="Options"
-                            display={{ base: 'inline-block', md: 'none' }}
-                        />
-                        <MenuList>
-                            <NextLink href="/about" passHref legacyBehavior>
-                                <MenuItem as={Link}>About</MenuItem>
-                            </NextLink>
-                            <NextLink href="/works" passHref legacyBehavior>
-                                <MenuItem as={Link}>Works</MenuItem>
-                            </NextLink>
-                            <NextLink href="/posts" passHref legacyBehavior>
-                                <MenuItem as={Link}>Posts</MenuItem>
-                            </NextLink>
-                        </MenuList>
-                    </Menu>
-                </Box>
-            </Container>
-        </Box>
+                    <Stack
+                        direction={{ base: 'column', md: 'row' }}
+                        display={{ base: 'none', md: 'flex' }}
+                        width={{ base: 'full', md: 'auto' }}
+                        alignItems="center"
+                        flexGrow={1}
+                        mt={{ base: 4, md: 0 }}
+                    >
+                        <LinkItem href="/about" path={path}>
+                            About
+                        </LinkItem>
+                        <LinkItem href="/works" path={path}>
+                            Works
+                        </LinkItem>
+                        <LinkItem href="/posts" path={path}>
+                            Posts
+                        </LinkItem>
+                    </Stack>
+
+                    <Box flex={1} align="right">
+                        <ThemeToggleButton />
+                        <Box ml={2} display={{ base: 'inline-block', md: 'none' }}>
+                            <Menu isLazy id="navbar-menu">
+                                <MenuButton
+                                    as={IconButton}
+                                    icon={<HamburgerIcon />}
+                                    variant="outline"
+                                    aria-label="Options"
+                                />
+                                <MenuList>
+                                    <NextLink href="/about" passHref legacyBehavior>
+                                        <MenuItem as={Link}>About</MenuItem>
+                                    </NextLink>
+                                    <NextLink href="/works" passHref legacyBehavior>
+                                        <MenuItem as={Link}>Works</MenuItem>
+                                    </NextLink>
+                                    <NextLink href="/posts" passHref legacyBehavior>
+                                        <MenuItem as={Link}>Posts</MenuItem>
+                                    </NextLink>
+                                </MenuList>
+                            </Menu>
+                        </Box>
+                    </Box>
+                </Container>
+            </Box>
         </>
     )
 }
