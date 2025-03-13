75d5c71923ba93a1c6948e4dc32f058a206fc074
johnhnguyen97 <john.nguyen7991@gmail.com>
2025-03-12 22:46:46 -0500

    Enhance 404 Not Found page layout and styling; improve button design and responsiveness

--- pages/404.js
+++ pages/404.js
@@ -1,23 +1,48 @@
 import NextLink from 'next/link'
 import {
-    Box, Heading, Text, Container, Divider, Button } from '@chakra-ui/react'
+    Box,
+    Heading,
+    Text,
+    Container,
+    Divider,
+    Button,
+    useColorModeValue
+} from '@chakra-ui/react'
 import Layout from '../components/layouts/article'
 
 const NotFound = () => {
     return (
         <Layout title="404 Not Found">
-            <Container>
-            <Heading as="h1">
-                Not Found
-            </Heading>
-            <Text> The page you&apos;re looking for was not found.</Text>
-            <Divider my={6} />
-
-            <Box my={6} align="center">
-                <NextLink href="/">
-                    <Button colorScheme="blue">Go back home</Button>
-                </NextLink>
-            </Box>
+            <Container pt={14}>
+                <Box
+                    borderRadius="lg"
+                    bg={useColorModeValue('rgba(218, 224, 228, 0.5)', 'rgba(218, 224, 228, 0.1)')}
+                    p={6}
+                    mb={6}
+                    align="center"
+                >
+                    <Heading as="h1" mb={4}>
+                        Not Found
+                    </Heading>
+                    <Text fontSize="lg" mb={6}>
+                        The page you&apos;re looking for was not found.
+                    </Text>
+                    <Divider my={6} />
+                    <Box align="center">
+                        <NextLink href="/" passHref>
+                            <Button
+                                bg={useColorModeValue('#2975A7', '#2975A7')}
+                                color={useColorModeValue('#DAE0E4', '#DAE0E4')}
+                                _hover={{
+                                    bg: useColorModeValue('#1c5a87', '#1c5a87')
+                                }}
+                                borderRadius="md"
+                            >
+                                Return to Home
+                            </Button>
+                        </NextLink>
+                    </Box>
+                </Box>
             </Container>
         </Layout>
     )
